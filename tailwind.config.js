d04f799d50ff3f22400c83dcbd3aae0014f45da8
threst22 <harry.timosa@gmail.com>
2025-06-22 09:24:27 +0800

    fix: final build configuration for Netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-        colors: {
-            orange: {
-                500: '#F53D2D', // Shopee Orange
-                600: '#D93627',
-                100: '#FFF0ED',
-            }
-        }
-    },
-  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
